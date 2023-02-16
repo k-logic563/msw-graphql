08c236973f01f318600d5ffd68f230da96c8df1b
k-logic24 <k.log.ic563@gmail.com>
2023-02-16 23:43:24 +0900

    wip

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,30 +1,10 @@
-import { graphql } from "msw";
+import { factory, primaryKey } from "@mswjs/data";
 
-export const handlers = [
-  graphql.mutation("Login", (req, res, ctx) => {
-    const { username } = req.variables;
+const db = factory({
+  post: {
+    id: primaryKey(String),
+    title: String,
+  },
+});
 
-    if (username === "no-user") {
-      return res(
-        ctx.errors([
-          {
-            message: "User not found",
-            extensions: {
-              id: "088677ef-253e-4018-9080-46b23176ed8d",
-            },
-          },
-        ])
-      );
-    }
-
-    return res(
-      ctx.data({
-        user: {
-          __typename: "User",
-          id: "088677ef-253e-4018-9080-46b23176ed8d",
-          name: username,
-        },
-      })
-    );
-  }),
-];
+export const handlers = [...db.post.toHandlers("graphql")];
